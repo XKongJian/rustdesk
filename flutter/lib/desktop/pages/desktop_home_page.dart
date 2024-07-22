6c6434fc8b859404dbfdf8e7ffd7546c3e5bf659
XKongJian <156444233+XKongJian@users.noreply.github.com>
2024-07-22 17:08:44 +0800

    Update desktop_home_page.dart

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -434,7 +434,14 @@ class _DesktopHomePageState extends State<DesktopHomePage>
           await rustDeskWinManager.closeAllSubWindows();
           bind.mainGotoInstall();
         });
-      } 
+      } else if (bind.mainIsInstalledLowerVersion()) {
+        return buildInstallCard(
+            "Status", "Your installation is lower version.", "Click to upgrade",
+            () async {
+          await rustDeskWinManager.closeAllSubWindows();
+          bind.mainUpdateMe();
+        });
+      }
     } else if (isMacOS) {
       final isOutgoingOnly = bind.isOutgoingOnly();
       if (!(isOutgoingOnly || bind.mainIsCanScreenRecording(prompt: false))) {
